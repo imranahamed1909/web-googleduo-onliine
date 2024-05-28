1373beafe4fa22996774b4beb93f9d77a36a5488
Zayn Yusuf <yusue786@gmail.com>
2024-05-28 11:41:27 +0600

    first commit

---
+++ app/connecting/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ConnectingPage() {
+  return (
+    <div className="text-2xl font-base text-center mt-10">Connecting....</div>
+  )
+}
+
+export default ConnectingPage
